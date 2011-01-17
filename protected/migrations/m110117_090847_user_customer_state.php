2d54c20270c935acd848a7add8a75851be81dacf
Ahmad Tanwir <th3crypt@gmail.com>
2011-01-17 17:11:28 +0700

    fixed migrate

--- protected/migrations/m110117_090847_user_customer_state.php
+++ protected/migrations/m110117_090847_user_customer_state.php
@@ -1,13 +1,15 @@
 <?php
 
+
+
 class m110117_090847_user_customer_state extends CDbMigration
 {
     public function up()
     {
         $this->addColumn('user','status','integer NOT NULL');
         $this->addColumn('customer','status','integer NOT NULL');
-        $this->update('user',array('status' => User::STATUS_ACTIVE));
-        $this->update('customer',array('status' => Customer::STATUS_ACTIVE));
+        $this->update('user',array('status' => 1));
+        $this->update('customer',array('status' => 1));
         
     }
 
