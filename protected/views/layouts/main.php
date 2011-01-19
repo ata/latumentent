4cf9c6ecc2a63ad851dd543529cf88517741d314
Ahmad Tanwir <th3crypt@gmail.com>
2011-01-19 19:14:55 +0700

    Add Image

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -34,7 +34,7 @@
 						array('label'=>'Home', 'url'=>array('/site/index')),
 						array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 						array('label'=>'Product', 'url'=>array('/site/page', 'view'=>'product')),
-						array('label'=>'Contact', 'url'=>array('/site/contact')),
+						array('label'=>'Contact', 'url'=>array('/site/contact',), 'linkOptions' => array('class' => 'last')),
 						//array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 						//array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 					),
