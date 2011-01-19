008900ea7f8a350b7d14f62c8394b06115366b95
Ahmad Tanwir <th3crypt@gmail.com>
2011-01-19 11:27:53 +0700

    aneh

--- protected/controllers/CustomerController.php
+++ protected/controllers/CustomerController.php
@@ -5,51 +5,51 @@
  */
 class CustomerController extends Controller
 {
-    public function actionIndex()
-    {
-        $customer = new Customer('search');
+	public function actionIndex()
+	{
+		$customer = new Customer('search');
 	
 	$this->render('index',array(
-	    'customer'=>$customer,
+		'customer'=>$customer,
 	));
-    }
+	}
 
-    public function actionCreate()
-    {
+	public function actionCreate()
+	{
 	
 	$customer = new Customer;
 	if($customer->user === null){
-	    $customer->user = new User;
+		$customer->user = new User;
 	}
 
 	if(isset($_POST['User'])){
-	    $customer->user->attributes = $_POST['User'];
-	    $customer->user->role_id = '2';
-	    if($customer->user->save()){
+		$customer->user->attributes = $_POST['User'];
+		$customer->user->role_id = '2';
+		if($customer->user->save()){
 		if(isset($_POST['Customer'])){
-		    $customer->attributes = $_POST['Customer'];
-		    $customer->user_id = Yii::app()->db->getLastInsertId();
-		    $customer->save();
-		    //var_dump($customer->attributes);
+			$customer->attributes = $_POST['Customer'];
+			$customer->user_id = Yii::app()->db->getLastInsertId();
+			$customer->save();
+			//var_dump($customer->attributes);
 		}
-	    }
+		}
 	}
 	$services = CHtml::listData(Service::model()->findAll(),'id','name');
-        $this->render('create',array(
-	    'customer'=>$customer,
-	    'services'=>$services,
-        ));
-    }
+		$this->render('create',array(
+			'customer'=>$customer,
+			'services'=>$services,
+		));
+	}
 
-    public function actionUpdate()
-    {
-        $customer = Customer::model()->findByPk($_GET['id']);
-        $services = CHtml::listData(Service::model()->findAll(),'id','name');
-        $this->render('update',array(
-               'customer'=>$customer,
-               'services'=>$services,
-            ));
-    }
+	public function actionUpdate()
+	{
+		$customer = Customer::model()->findByPk($_GET['id']);
+		$services = CHtml::listData(Service::model()->findAll(),'id','name');
+		$this->render('update',array(
+			'customer'=>$customer,
+			'services'=>$services,
+		));
+	}
 
 
 }
