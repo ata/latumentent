2cd1fd1d93abdf99e20552a7d45609416f60c4bb
unknown <Erika@.(none)>
2011-02-23 10:42:37 +0700

    perubahan warna konten pages u Please enter the commit message for your changes. Lines starting

--- protected/views/site/pages/product.php
+++ protected/views/site/pages/product.php
@@ -4,27 +4,36 @@ $this->breadcrumbs=array(
 	'Product',
 );
 ?>
-<h1>Product</h1>
+<div class="page-content">
+	<div class="page-content-title">
+	<h1>Product</h1>
+	</div>
+	<div class="page-content-text">
+		<p>Apartments.com recently conducted a survey to current renters about decorating, 
+		ninety percent of renters will decorate when moving to a new apartment; and of these 
+		renters, eighty-one percent will decorate within the first three months of their move.</p>
 
-<p>Apartments.com recently conducted a survey to current renters about decorating, 
-ninety percent of renters will decorate when moving to a new apartment; and of these 
-renters, eighty-one percent will decorate within the first three months of their move.</p>
+		<p>Most renters said that their biggest decorating challenge is staying within a budget (38%). 
+		Other survey respondents stated their challenges when decorating include: not having enough 
+		space (23%), need help with organization (12%) and unsure of what colors to use (12%).</p>
 
-<p>Most renters said that their biggest decorating challenge is staying within a budget (38%). 
-Other survey respondents stated their challenges when decorating include: not having enough 
-space (23%), need help with organization (12%) and unsure of what colors to use (12%).</p>
+		<p>In our survey, we also found that the majority of renters, sixty-five percent, 
+		plan to spend $500 or less on their decorating budget while eighteen percent of 
+		respondents will spend between $500 and $1000.</p>
 
-<p>In our survey, we also found that the majority of renters, sixty-five percent, 
-plan to spend $500 or less on their decorating budget while eighteen percent of 
-respondents will spend between $500 and $1000.</p>
+		<p>But decorating does not have to break the bank, as Eva the Shopping Diva, 
+		shopping expert and blogger from ShopLocal, offers this advice for renters decorating on a budget:</p>
 
-<p>But decorating does not have to break the bank, as Eva the Shopping Diva, 
-shopping expert and blogger from ShopLocal, offers this advice for renters decorating on a budget:</p>
+		<p>First try a small yet important room, like the bathroom. Simply hanging a new shower curtain with 
+		a gorgeous pattern can really change the look of the room. Add a great bath mat that compliments 
+		the pattern and a new look is born.</p>
+		<p>Re-covering old dining room chairs or a worn-out ottoman are easy ways to subtly change the 
+		style of a room. Select a fabulous fabric and use a staple gun to give old furniture a new life.</p>
+		<p>Wall decals are a snap to apply and if you get tired of them you can easily peel them off. 
+		You can get very creative with this great alternative to wallpaper.</p>
+	</div>
+</div>
 
-<p>First try a small yet important room, like the bathroom. Simply hanging a new shower curtain with 
-a gorgeous pattern can really change the look of the room. Add a great bath mat that compliments 
-the pattern and a new look is born.</p>
-<p>Re-covering old dining room chairs or a worn-out ottoman are easy ways to subtly change the 
-style of a room. Select a fabulous fabric and use a staple gun to give old furniture a new life.</p>
-<p>Wall decals are a snap to apply and if you get tired of them you can easily peel them off. 
-You can get very creative with this great alternative to wallpaper.</p>
+
+
+
