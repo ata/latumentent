39a3c576bd37700b20cdf6a623c6ed1d56e28089
unknown <Erika@.(none)>
2011-02-28 09:28:07 +0700

    tampilan awal

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,33 +1,6 @@
-<div id="slideshow" class="span-24">
-	<div class="image-container">
-		<img src="<?php echo Yii::app()->request->baseUrl?>/images/front-image.jpg" alt="image" title="image" width="910px" />
-	</div>
-	<div class="info">
-		<div class="row">
-			<span class="floatLeft title">Room Type:</span>
-			<span class="floatRight value">Duluxe</span>
-			<div class="clear"></div>
-		</div>
-		<div class="row">
-			<span class="floatLeft title">Land Area:</span>
-			<span class="floatRight value">2800 sq/ft</span>
-			<div class="clear"></div>
-		</div>
-		<div class="row">
-			<span class="floatLeft title">Baths:</span>
-			<span class="floatRight value">2</span>
-			<div class="clear"></div>
-		</div>
-		<div class="row">
-			<span class="floatLeft title">Beds:</span>
-			<span class="floatRight value">3</span>
-			<div class="clear"></div>
-		</div>
-	</div>
-</div>
 
 <div class="content">
-	<div class="posts span-15 colborder">
+	<div class="posts span-16">
 		<div class="post">
 			<h2 class="ptitle">Recent News</h2>
 			<div class="pcontent">
@@ -90,20 +63,22 @@
 	</div>
 
 	<div class="span-8 last sidebar">
-		<h2 class="stitle">Menu Sidebar</h2>
-		<ul class="scontent">
-			<li>First try a small yet important room, like the bathroom</li>
-			<li>Re-covering old dining room chairs or a worn-out ottoman are easy ways to subtly change the style of a room</li>
-			<li>Wall decals are a snap to apply and if you get tired of them you can easily peel them off</li>
-			<li>A tasting comparison, sample flights of the same kind of wine</li>
-			<li>A regional theme, like Australian Whites or Italian Reds</li>
-			<li>Wall decals are a snap to apply and if you get tired of them you can easily peel them off</li>
-			<li>A tasting comparison, sample flights of the same kind of wine</li>
-			<li>A regional theme, like Australian Whites or Italian Reds</li>
-			<li>Wall decals are a snap to apply and if you get tired of them you can easily peel them off</li>
-			<li>A tasting comparison, sample flights of the same kind of wine</li>
-			<li>A regional theme, like Australian Whites or Italian Reds</li>
-		</ul>
+		<div class="scontent">
+			<h2 class="stitle">Testimonials</h2>
+			<ul class="testimonial-content">
+				<li>First try a small yet important room, like the bathroom</li>
+				<li>Re-covering old dining room chairs or a worn-out ottoman are easy ways to subtly change the style of a room</li>
+				<li>Wall decals are a snap to apply and if you get tired of them you can easily peel them off</li>
+				<li>A tasting comparison, sample flights of the same kind of wine</li>
+				<li>A regional theme, like Australian Whites or Italian Reds</li>
+				<li>Wall decals are a snap to apply and if you get tired of them you can easily peel them off</li>
+				<li>A tasting comparison, sample flights of the same kind of wine</li>
+				<li>A regional theme, like Australian Whites or Italian Reds</li>
+				<li>Wall decals are a snap to apply and if you get tired of them you can easily peel them off</li>
+				<li>A tasting comparison, sample flights of the same kind of wine</li>
+				<li>A regional theme, like Australian Whites or Italian Reds</li>
+			</ul>
+		</div>
 	</div>
 </div>
 
--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -4,38 +4,39 @@ $this->breadcrumbs=array(
 	'Login',
 );
 ?>
+<div id="login">
+	<h2 id="login-title"><?php echo Yii::t('app','Login')?></h2>
 
-<h2 id="login-title"><?php echo Yii::t('app','Login')?></h2>
+	<div class="form">
+	<?php $form=$this->beginWidget('CActiveForm', array(
+		'id'=>'login-form',
+		'enableAjaxValidation'=>true,
+	)); ?>
 
-<div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableAjaxValidation'=>true,
-)); ?>
+		<p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+		<div class="row">
+			<?php echo $form->labelEx($model,'username'); ?>
+			<?php echo $form->textField($model,'username'); ?>
+			<?php echo $form->error($model,'username'); ?>
+		</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+		<div class="row">
+			<?php echo $form->labelEx($model,'password'); ?>
+			<?php echo $form->passwordField($model,'password'); ?>
+			<?php echo $form->error($model,'password'); ?>
+		</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-	</div>
+		<div class="row rememberMe">
+			<?php echo $form->label($model,'rememberMe'); ?>
+			<?php echo $form->checkBox($model,'rememberMe'); ?>
+			<?php echo $form->error($model,'rememberMe'); ?>
+		</div>
 
-	<div class="row rememberMe">
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
+		<div class="row buttons">
+			<?php echo CHtml::submitButton('Login'); ?>
+		</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
-</div><!-- form -->
+	<?php $this->endWidget(); ?>
+	</div><!-- form -->
+</div>
