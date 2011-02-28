55e05b15567abaf488f6f41516115d64665ee080
didin <deannurdiansyah@gmail.com>
2011-02-28 17:54:49 +0700

    service package

--- protected/views/admin/servicePackage/update.php
+++ protected/views/admin/servicePackage/update.php
@@ -14,4 +14,7 @@ $this->menu=array(
 
 <h2><?php echo Yii::t('app','Update {name}',array('{name}' => $servicePackage->name)); ?></h2>
 
-<?php echo $this->renderPartial('_form', array('servicePackage'=>$servicePackage)); ?>
+<?php echo $this->renderPartial('_form', array(
+	'servicePackage'=>$servicePackage,
+	'serviceList'=>$serviceList,
+	)); ?>
