e2242fafefaede30db508f55c726c5a2de5d1302
unknown <Erika@.(none)>
2011-03-02 11:24:36 +0700

    table dan warna

--- protected/views/invoice/_total.php
+++ protected/views/invoice/_total.php
@@ -1,17 +1,17 @@
 <table>
 	<thead>
 		
-		<tr>
+		<tr class="pbills">
 			<th class="title"><?php echo Yii::t('app','Total Bills have Paid')?></th>
 			<th class="ar value"><?php echo $totalPaidBill ?></th>
 		</tr>
 		
-		<tr>
+		<tr class="ybills">
 			<th class="title"><?php echo Yii::t('app','Total Paid Bills have\'t Paid')?></th>
 			<th class="ar value"><?php echo $totalNotPaidBill ?></th>
 		</tr>
 		
-		<tr>
+		<tr class="tbills">
 			<th class="title"><?php echo Yii::t('app','Total Bills')?></th>
 			<th class="ar value"><?php echo $totalBill ?></th>
 		</tr>
