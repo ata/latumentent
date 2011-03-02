e067950edc8bad3554a3c2595de89ea926a40067
unknown <Erika@.(none)>
2011-03-02 11:35:35 +0700

    warna

--- protected/views/revenue/_total.php
+++ protected/views/revenue/_total.php
@@ -1,6 +1,6 @@
 <table>
 	<thead>
-		<tr>
+		<tr class="trevenue">
 			<th class="title"><?php echo Yii::t('app','Total Revenue')?></th>
 			<th class="ar value"><?php echo $totalRevenue?></th>
 		</tr>
